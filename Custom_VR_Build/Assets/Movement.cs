d5419161fe0cc39f11116e736ec7ce376d5d1f51
Eli <eli.p561@stu.nebo.edu>
2021-03-17 10:01:05 -0600

    Simple VR build

---
+++ Custom_VR_Build/Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed = 10;
+    private Transform cam;
+    private float lr;
+    private float fb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = transform.GetChild(0).GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, cam.rotation.y, transform.rotation.z));
+        fb = Input.GetAxis("Vertical");
+        lr = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * fb * Time.deltaTime * speed);
+        transform.Translate(Vector3.left * lr * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+            cam.Rotate(Vector3.forward * speed * 5 * Time.deltaTime);
+    }
+}
